0dcfaeaaf4a3bb344830d58dbff44777887d378f
LeeSungmin <lsmin01@gmail.com>
2020-01-13 00:34:24 +0900

    Fix Error

--- src/main/java/com/lsmin01/book/springboot/Application.java
+++ src/main/java/com/lsmin01/book/springboot/Application.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 
 
-@EnableJpaAuditing
+//@EnableJpaAuditing
 @SpringBootApplication
 public class Application {
     public static void main(String[] args){
