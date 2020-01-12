92713579220e5434f7844e3276df33d2f7c97c89
LeeSungmin <lsmin01@gmail.com>
2020-01-12 19:24:00 +0900

    first commit

---
+++ src/main/java/com/lsmin01/book/springboot/web/HelloController.java
@@ -0,0 +1,20 @@
+package com.lsmin01.book.springboot.web;
+
+import com.lsmin01.book.springboot.web.Dto.HelloResponseDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String Hello(){
+        return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto HelloDto(@RequestParam("name") String name,
+                                     @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
+    }
+}
